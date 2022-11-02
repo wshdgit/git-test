0de9de4128f1e7978e929b10b86b7c8639e4b30f
he <he@love.cn>
2022-11-02 22:44:54 +0800

    hot-fix commit2

--- src/main/java/indi/mrhe/git/GitTest.java
+++ src/main/java/indi/mrhe/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git2!");
         System.out.println("hello git3!");
         System.out.println("hello fix4");
+        System.out.println("hello fix5");
     }
 }
