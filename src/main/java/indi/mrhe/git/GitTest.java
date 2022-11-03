acd197ec1b8b08a073540ce4eede64723c416fc9
wshdgit <103836904+wshdgit@users.noreply.github.com>
2022-11-03 11:38:52 +0800

    pull commit

--- src/main/java/indi/mrhe/git/GitTest.java
+++ src/main/java/indi/mrhe/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello master5");
         System.out.println("hello fix5");
         System.out.println("hello push");
+        System.out.println("hello pull");
     }
 }
