483f35c7df7a1a599bd9a6c83ae2e7f525e9ad10
wshdgit <103836904+wshdgit@users.noreply.github.com>
2022-12-06 12:14:36 +0800

    Remote Push

--- src/main/java/indi/mrhe/git/GitTest.java
+++ src/main/java/indi/mrhe/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello push");
         System.out.println("hello pull");
         System.out.println("test1");
+        System.out.println("Remote Push");
     }
 }
